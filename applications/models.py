b6532187f68b7b52dcff59301d69addd14a0869c
ziabluk <teretoriaz5467@gmail.com>
2022-12-26 14:25:47 +0300

    Add Application

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    date_take = models.DateField() #хранит дату когда возьмет
+    date_give = models.DateField() #хранит дату когда отдаст
+    comment = models.CharField(max_length=200)
+
+
